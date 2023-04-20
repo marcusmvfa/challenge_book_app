3b0e4cce538e02a60afd668e5296d062073cd043
Marcus Vinicius da Fonseca Antunes <43256442+marcusmvfa@users.noreply.github.com>
2023-04-20 17:49:37 -0300

    remove key

--- lib/domain/constants/constants.dart
+++ lib/domain/constants/constants.dart
@@ -9,6 +9,6 @@ enum OrderBy {
 }
 
 class Constants {
-  static String apiKey = "AIzaSyDmRHMg2Xxaewa5WJWeSd49H_5w0Fx6w_s";
+  static String apiKey = "your_api_goes_here";
   static String volumesURL = "https://www.googleapis.com/books/v1/volumes";
 }
